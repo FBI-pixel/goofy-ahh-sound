84ef7f4446a0e26f41de30cdfbb1a7a2b616e4a2
FBI-pixel <71829266+FBI-pixel@users.noreply.github.com>
2023-10-10 07:57:51 -0300

    a

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+document.querySelectorAll('.tecla');
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0] .onclick = tocaSomPom;
