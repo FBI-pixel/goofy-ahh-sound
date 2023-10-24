2123689b0651cbcf66067d7559ae7b55d079ebdf
FBI-pixel <71829266+FBI-pixel@users.noreply.github.com>
2023-10-24 07:43:11 -0300

    adada

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
@@ -10,7 +10,7 @@ listaDeTeclas[0].onclick = tocaSomPom;
 
 let contador = 0;
 
-while(contador < 9){
+while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = tocaSomPom;
     contador = contador + 1;
     console.log(contador);
