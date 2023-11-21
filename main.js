019957d206febc112471068e4fc1066d30219c0f
FBI-pixel <71829266+FBI-pixel@users.noreply.github.com>
2023-11-21 07:28:08 -0300

    adaasdfsegdvasdafdvsd

--- main.js
+++ main.js
@@ -6,11 +6,19 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSom;
 
-for(let contador = 0;contador < listaDeTeclas.length;contador++){
+for(let contador = 0;contador < listaDeTeclas.length;contador++);
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1]
     const idAudio = ´#som_$(efeito)´;
     tecla.onclick = function(){
         tocaSom(idAudio)
     }
+    tecla.onkeydown = function (evento){
+      if (EventSource.code != 'tab'){
+        tecla.classList.add('ativa');
+      }
+      tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+      }
+          
 }
